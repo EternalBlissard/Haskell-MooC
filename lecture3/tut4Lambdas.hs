2231b8b59551aaedcaec1605265138e8567b4def
Chaitanya Garg <chaitanya21248@iiitd.ac.in>
2024-04-17 20:12:03 +0530

    Lambda Functions

---
+++ lecture3/tut4Lambdas.hs
@@ -0,0 +1,10 @@
+module Lambda where
+    -- Lambda functions are anonymous functions that are used because they are short and simple
+    -- They are written as \arg1 arg2 ... argN -> body
+    -- They can be used in place of a named function
+    -- They can be passed as arguments to higher order functions
+    -- They can be returned as values from higher order functions
+    -- They can be used to create functions that are not needed elsewhere or are used only once
+    -- Its never needed to use a lambda expression. You can always instead define the function normally using let or where.
+main = do
+    print((\x -> x + 3) 5)
